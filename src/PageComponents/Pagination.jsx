d8f29245924be379a27d7942f788f58565fa571a
saurabh_joshi24 <sjoshi@quantek.solutions>
2024-07-11 17:53:26 +0530

    feat(dashboard): added disabled prop and css logic for previous and next button in pagination

--- src/PageComponents/Pagination.jsx
+++ src/PageComponents/Pagination.jsx
@@ -17,11 +17,15 @@ const StyledUl = styled.ul`
     justify-content: center;
 `
 
-const StyledLi = styled.li `
+const StyledLi = styled.li`
     min-width: 30px;
     background: #fff;
     border: 1px solid #ddd;
     margin: 0px 5px 0px;
+    color: ${props => props.disabled ? 'gray' : 'inherit'};
+    cursor: ${props => props.disabled ? 'not-allowed' : 'pointer'};
+    opacity: ${props => props.disabled ? 0.7 : 1};
+    pointer-events: ${props => props.disabled ? 'none' : 'auto'};
 `
 
 const StyledButton = styled.button`
@@ -33,79 +37,82 @@ const StyledButton = styled.button`
 `
 
 const Pagination = () => {
-  const dispatch = useDispatch();
-  const { currentPage, totalPages, pageLimit , itemsPerPage } = useSelector((state) => state.pagination.value);
+    const dispatch = useDispatch();
+    const { currentPage, totalPages, pageLimit, itemsPerPage } = useSelector((state) => state.pagination.value);
 
 
-  useEffect(() => {
-      const lastItemIndex = currentPage * itemsPerPage;
-      const firstItemIndex = lastItemIndex - itemsPerPage;
+    useEffect(() => {
+        const lastItemIndex = currentPage * itemsPerPage;
+        const firstItemIndex = lastItemIndex - itemsPerPage;
 
-      dispatch(setFirstIndex(firstItemIndex));
-      dispatch(setLastIndex(lastItemIndex));
+        dispatch(setFirstIndex(firstItemIndex));
+        dispatch(setLastIndex(lastItemIndex));
 
-  }, [currentPage, itemsPerPage])
+    }, [currentPage, itemsPerPage])
 
-  // handles page change on page number click
-  const handleClick = (page) => {
-    dispatch(setCurrentPage(page));
-  };
+    // handles page change on page number click
+    const handleClick = (page) => {
+        dispatch(setCurrentPage(page));
+    };
 
-  // handles next page
-  const handleNextPage = () => {
-    if (currentPage < totalPages) {
-      const nextPage = currentPage + 1;
-      dispatch(setCurrentPage(nextPage));
-    }
-  };
+    // handles next page
+    const handleNextPage = () => {
+        if (currentPage < totalPages) {
+            const nextPage = currentPage + 1;
+            dispatch(setCurrentPage(nextPage));
+        }
+    };
 
-  // handles previous page
-  const handlePrevPage = () => {
-    if (currentPage > 1) {
-      const prevPage = currentPage - 1;
-      dispatch(setCurrentPage(prevPage));
-    }
-  };
+    // handles previous page
+    const handlePrevPage = () => {
+        if (currentPage > 1) {
+            const prevPage = currentPage - 1;
+            dispatch(setCurrentPage(prevPage));
+        }
+    };
 
-  //  This generated page numbers array in pagination items
-  const renderPageNumbers = () => {
-    const pageNumbers = [];
+    //  This generated page numbers array in pagination items
+    const renderPageNumbers = () => {
+        const pageNumbers = [];
 
-    let startPage = Math.max(1, currentPage - Math.floor(pageLimit / 2));
-    let endPage = Math.min(totalPages, startPage + pageLimit - 1);
+        let startPage = Math.max(1, currentPage - Math.floor(pageLimit / 2));
+        let endPage = Math.min(totalPages, startPage + pageLimit - 1);
 
-    if (totalPages > 1) {
-      for (let i = startPage; i <= endPage; i++) {
-        pageNumbers.push(
-          <StyledLi key={i} >
-            <StyledButton onClick={() => handleClick(i)}>
-              {i}
-            </StyledButton>
-          </StyledLi>
-        );
-      }
-    }
+        if (totalPages > 1) {
+            for (let i = startPage; i <= endPage; i++) {
+                pageNumbers.push(
+                    <StyledLi
+                        key={i}
+                        active={currentPage === i}
+                    >
+                        <StyledButton onClick={() => handleClick(i)}>
+                            {i}
+                        </StyledButton>
+                    </StyledLi>
+                );
+            }
+        }
 
-    return pageNumbers;
-  };
+        return pageNumbers;
+    };
 
-  return (
-    <StyledNav>
-        <StyledUl>
-          <StyledLi>
-            <StyledButton onClick={handlePrevPage}>
-              Previous
-            </StyledButton>
-          </StyledLi>
-          {renderPageNumbers()}
-          <StyledLi>
-            <StyledButton onClick={handleNextPage}>
-              Next
-            </StyledButton>
-          </StyledLi>
-        </StyledUl>
-    </StyledNav>
-  );
+    return (
+        <StyledNav>
+            <StyledUl>
+                <StyledLi disabled={currentPage === 1}>
+                    <StyledButton onClick={handlePrevPage}>
+                        Previous
+                    </StyledButton>
+                </StyledLi>
+                {renderPageNumbers()}
+                <StyledLi disabled={currentPage === totalPages}>
+                    <StyledButton onClick={handleNextPage}>
+                        Next
+                    </StyledButton>
+                </StyledLi>
+            </StyledUl>
+        </StyledNav>
+    );
 };
 
 export default Pagination;
