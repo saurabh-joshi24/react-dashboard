cb19e4c903690ae94cee4d625b2c07cc943b272a
saurabh_joshi24 <sjoshi@quantek.solutions>
2024-07-11 13:58:32 +0530

    feat(dashboard): added getCountries api for fetching data in app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import DataTable from './PageComponents/DataTable';
+import { useEffect } from 'react';
+import getCountries from "./api/getCountries";
+import { useDispatch } from 'react-redux';
+import { setCountries } from './redux/countries';
+import DashboardCards from './PageComponents/DashboardCards';
 
 function App() {
+  const dispatch = useDispatch();
+
+  const fetchData = async() =>  {
+    const data = await getCountries();
+    dispatch(setCountries(data))
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>React Dashboard</h1>
       </header>
+      <main>
+        <DashboardCards/>
+        <DataTable/>
+      </main>
     </div>
   );
 }
