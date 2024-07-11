ba10bcf2eacc7e0af137d9504abb55f714ff7165
saurabh_joshi24 <sjoshi@quantek.solutions>
2024-07-11 16:58:32 +0530

    feat(dashboard): added pagination.jsx component with styled components

---
+++ src/PageComponents/Pagination.jsx
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+const StyledNav = styled.nav`
+    display: flex;
+    width: 90%;
+    justify-content: center;
+    margin: 0 auto;
+    margin-top: 20px;
+    margin-bottom:
+`
+const StyledUl = styled.ul`
+    display: flex;
+    list-style: none;
+    justify-content: center;
+`
+
+const StyledLi = styled.li `
+
+`
+
+const StyledButton = styled.button`
+
+`
