8ae7fc6c97b694efb69807f94536704452f67a36
saurabh_joshi24 <sjoshi@quantek.solutions>
2024-07-11 10:48:31 +0530

    feat(dashboard): added card.jsx reusable component in src/components

---
+++ src/components/card.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const Card = styled.div`
+  border-radius: 10px;
+  min-width: 250px;
+  background-color: #ffffff;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  padding: 20px;
+  margin: 10px;
+  cursor: pointer;
+`
+
+export { Card }
