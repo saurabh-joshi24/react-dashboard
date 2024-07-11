495553090cf3f68b30ae099b782a1d5275095897
saurabh_joshi24 <sjoshi@quantek.solutions>
2024-07-11 17:19:53 +0530

    feat(dashboard): added pagination slice in redux store.js

--- src/store.js
+++ src/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
 import countries from './redux/countries'
+import pagination from './redux/pagination'
 
 export default configureStore({
   reducer: {
-    countries
+    countries,
+    pagination
   },
 })
