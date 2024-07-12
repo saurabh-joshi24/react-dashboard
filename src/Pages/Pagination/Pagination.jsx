a8b0561c2e245b025452fb1bf8d0ada44a78dc8f
saurabh_joshi24 <sjoshi@quantek.solutions>
2024-07-12 10:22:23 +0530

    feat(dashboard): moved Pagination.js component to Pages/Pagination folder

--- src/Pages/Pagination/Pagination.jsx
+++ src/Pages/Pagination/Pagination.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { setCurrentPage, setLastIndex, setFirstIndex } from "../redux/pagination";
+import { setCurrentPage, setLastIndex, setFirstIndex } from "../../redux/pagination";
 import styled from "styled-components";
 
 const StyledNav = styled.nav`
