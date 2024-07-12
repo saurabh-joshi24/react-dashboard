f6916e8ec0d2945a67384c5c0fdc41e23c5c54d4
saurabh_joshi24 <sjoshi@quantek.solutions>
2024-07-12 11:47:21 +0530

    feat(dashboard): removed pagination redux slice from redux store

--- src/store.js
+++ src/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
 import countries from './redux/countries'
-import pagination from './redux/pagination'
 
 export default configureStore({
   reducer: {
-    countries,
-    pagination
+    countries
   },
 })
