a885e27fa4123510b94f077177421c13cfe11ae8
saurabh_joshi24 <sjoshi@quantek.solutions>
2024-07-12 18:24:02 +0530

    feat(dashboard): integrated other country selectors in app.js

--- src/App.js
+++ src/App.js
@@ -8,16 +8,20 @@ import Pagination from './Pages/Pagination/Pagination';
 import DashboardCards from './Pages/DashboardCards/DashboardCards';
 import { COUNTRY_COLUMNS } from './constants/countries';
 import { INITIAL_PAGINATION_DATA } from './constants/pagination';
+import { selectAllCountries, selectCountriesByPagination, selectTopCountriesByPopulation } from './selectors/countries';
+import { renderCardContent } from './utils/dashboardCard';
 
 
 function App() {
   const dispatch = useDispatch();
-
-  const countries = useSelector((state) => state.countries.value)
-
   const [paginationData, setPaginationData] = useState(INITIAL_PAGINATION_DATA);
-
   const { itemsPerPage, indexOfFirstItem, indexOfLastItem } = paginationData;
+
+  const countriesByPagination = selectCountriesByPagination(indexOfFirstItem, indexOfLastItem)
+  
+  const paginatedCountries = useSelector(countriesByPagination);
+  const topCountriesByPopulation = useSelector(selectTopCountriesByPopulation);
+  const allCountries = useSelector(selectAllCountries);
 
   const fetchData = async () => {
     const data = await getCountries();
@@ -29,9 +33,9 @@ function App() {
   }
 
   useEffect(() => {
-    const totalPagesCount = Math.ceil(countries.length / itemsPerPage);
+    const totalPagesCount = Math.ceil(allCountries.length / itemsPerPage);
     setPaginationData({ ...paginationData, totalPages: totalPagesCount });
-  }, [itemsPerPage])
+  }, [itemsPerPage, allCountries])
 
   useEffect(() => {
     fetchData();
@@ -47,8 +51,8 @@ function App() {
         <h1>React Dashboard</h1>
       </header>
       <main>
-        <DashboardCards />
-        <DataTable datalist={countries.slice(indexOfFirstItem, indexOfLastItem)} columns={COUNTRY_COLUMNS} />
+        <DashboardCards datalist={topCountriesByPopulation} renderCardContent={renderCardContent} />
+        <DataTable datalist={paginatedCountries} columns={COUNTRY_COLUMNS} />
         <Pagination {...paginationData} onChange={onPaginationChange} />
       </main>
     </div>
