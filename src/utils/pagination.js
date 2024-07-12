d64f97eaba6fd7ecf8ae8075d62e9570e5f8ab43
saurabh_joshi24 <sjoshi@quantek.solutions>
2024-07-12 10:58:33 +0530

    feat(dashboard): added PageNumbers function for rendering page numbers in utils/pagination.js

---
+++ src/utils/pagination.js
@@ -0,0 +1,30 @@
+import { StyledLi, StyledButton } from "../Pages/Pagination/Paginations.styles";
+
+//  This generates page numbers array in pagination items
+const PageNumbers = ({ currentPage, totalPages, pageLimit, handleClick }) => {
+    const pageNumbers = [];
+
+    let startPage = Math.max(1, currentPage - Math.floor(pageLimit / 2));
+    let endPage = Math.min(totalPages, startPage + pageLimit - 1);
+
+    if (totalPages > 1) {
+        for (let i = startPage; i <= endPage; i++) {
+            pageNumbers.push(
+                <StyledLi
+                    key={i}
+                    active={currentPage === i}
+                >
+                    <StyledButton
+                        active={currentPage === i}
+                        onClick={() => handleClick(i)}>
+                        {i}
+                    </StyledButton>
+                </StyledLi>
+            );
+        }
+    }
+
+    return pageNumbers;
+};
+
+export { PageNumbers }
