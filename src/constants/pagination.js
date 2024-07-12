f451ca2c474bae27509c4992eb4c70d44c2e9606
saurabh_joshi24 <sjoshi@quantek.solutions>
2024-07-12 13:22:05 +0530

    feat(dashboard): added pagination.js constant file in src/constants

---
+++ src/constants/pagination.js
@@ -0,0 +1,10 @@
+const INITIAL_PAGINATION_DATA = {
+    currentPage: 1,
+    totalPages: 0,
+    pageLimit: 5,
+    itemsPerPage: 20,
+    indexOfLastItem: 20,
+    indexOfFirstItem: 0
+  }
+
+export { INITIAL_PAGINATION_DATA }
