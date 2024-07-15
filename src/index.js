3d9a232f3e3d467e4b1f0d6aced88a61937aa95d
saurabh_joshi24 <sjoshi@quantek.solutions>
2024-07-15 12:58:53 +0530

    feat(dashboard): setup react query client in index.js

--- src/index.js
+++ src/index.js
@@ -3,14 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import store from './store'
 import { Provider } from 'react-redux'
+
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </Provider>
   </React.StrictMode>
 );
