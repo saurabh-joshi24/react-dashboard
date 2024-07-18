20139538bcd00bf3d6298333353a5c68f99ddb3e
saurabh_joshi24 <sjoshi@quantek.solutions>
2024-07-18 09:51:48 +0530

    feat(dashboard): fixed store import in index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import store from './store'
+import store from '@/store'
 import { Provider } from 'react-redux'
 
 const queryClient = new QueryClient();
